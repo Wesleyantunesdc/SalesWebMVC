131bdf53b66de8ab916bb15315a60ca4f9871a0e
Wesleyantunesdc <wesleyantunesdca@gmail.com>
2022-06-16 18:47:17 -0300

    First Model-Controller-View - Department

---
+++ SalesWebMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento {Id = 1, Nome = "Eletronicos" });
+            list.Add(new Departamento { Id = 2, Nome = "Moda" });
+            return View(list);
+        }
+    }
+}
